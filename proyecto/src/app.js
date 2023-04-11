1ee7b5a14daba9dc4d84768060e70e514922c7b6
elrikikiki <santizanini1@gmail.com>
2023-04-11 15:53:40 -0300

    agregado el handlebars falta el socket

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -1,12 +1,28 @@
 import express from "express";
 import prodRouter from "./routes/prodRouter.js";
 import cartRouter from "./routes/cartRouter.js";
+import {engine} from 'express-handlebars'
+import {resolve} from 'path'
 const app = express()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
+
+const viewsPath = resolve('./src/views')
+app.engine('handlebars',engine({
+    layoutsDir:`${viewsPath}/layouts`,
+    defaultLayout:`${viewsPath}/layouts/home.handlebars`
+}));
+app.set('view engine', 'handlebars')
+app.set('views', viewsPath)
+app.get('/handle',function(req,res){ //ojo con esa ruta
+res.render({name:'Santi', title:'My Page'})
+})
+
+
+
 app.use('/api/products',prodRouter)
 app.use('/api/carts',cartRouter)
 app.listen(8082,()=> {
     console.log('listen on port 8082');
-})
+});
